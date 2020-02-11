109028d943be0b89e41000a29bdda51494826cfa
Fitrahtur Rahman <fitrahtur@gmail.com>
2020-02-11 15:18:23 +0700

    add text announcement for floods

--- server/src/helpers/announcerTranscript.js
+++ server/src/helpers/announcerTranscript.js
@@ -7,8 +7,8 @@ function announcerTranscript(disaster, payload) {
       text = `Mohon perhatian. Kami dari BMKG menginformasikan bahwa telah terjadi gempa bumi sebesar ${magnitude} Skala Rihter pada pukul 8 Waktu Indonesia Barat, dengan kedalaman ${depth} kilometer, di ${earthquake_point}. Dihimbau kepada warga agar waspada terhadap ${aftershocks ? 'gempa susulan' : ''}${tsunami ? ' dan bahaya tsunami' : ''}. Terima kasih.`
     case 'flood':
       const { lock, status, time, water_level, weather } = payload
+      text = `Mohon perhatian. Kami dari BPBD Provinsi DKI Jakarta menginformasikan bahwa pintu air ${lock} memasuki ${status} pada ${time} WIB dengan ketinggian ${water_level} sentimeter, ${weather}. Dihimbau kepada warga Bantaran Kali agar waspada banjir kiriman. Terima kasih.`
   }
-
   return text
 }
 
