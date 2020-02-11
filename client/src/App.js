b9647f1a47fe9b60c570f676c7b8d0e5cf5dcf27
sukmo <sukmo@qlue.id>
2020-02-11 17:14:03 +0700

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
 
 function App() {
+  const position = [51.505, -0.09]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{height: "100%",width:"100%"}}>
+      <Map style={{height:'80vh'}} center={position} zoom={13}>
+      <TileLayer
+        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+      </Map>
     </div>
   );
 }
+
 
 export default App;
