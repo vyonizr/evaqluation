1613454482f92b0976570ab868911095e8ef0684
Fitrahtur Rahman <fitrahtur@gmail.com>
2020-02-11 20:56:14 +0700

    add firebase to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet'
+import * as firebase from "firebase/app";
+import 'firebase/auth'
+import 'firebase/firestore'
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
-  const position = [51.505, -0.09]
+  const position = [-0.8, 120.9213]
   return (
     <div style={{height: "100%",width:"100%"}}>
-      <Map style={{height:'80vh'}} center={position} zoom={13}>
+      <LeafletMap style={{height:'80vh'}} center={position} zoom={4}>
       <TileLayer
         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -16,7 +21,7 @@ function App() {
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
       </Marker>
-      </Map>
+      </LeafletMap>
     </div>
   );
 }
