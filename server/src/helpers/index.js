6244de4d7a6d62e6b7b556f525313b23c5010af1
Fitrahtur Rahman <fitrahtur@gmail.com>
2020-02-11 15:17:16 +0700

    add post method for creating water lock data

--- server/src/helpers/index.js
+++ server/src/helpers/index.js
@@ -1,7 +1,9 @@
 const announcerTranscript = require('./announcerTranscript')
 const textToSpeech = require('./textToSpeech')
+const locksData = require('./dummyLocks.json')
 
 module.exports = {
   announcerTranscript,
-  textToSpeech
+  textToSpeech,
+  locksData
 }
--- server/src/routes/floods.js
+++ server/src/routes/floods.js
@@ -2,5 +2,6 @@ const router = require('express').Router()
 const { Flood } = require('../controllers')
 
 router.get('/', Flood.getAllLocksData)
+router.post('/', Flood.createNewWaterLockInfo)
 
 module.exports = router
