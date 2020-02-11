eb4eb22311ac82347bbbe7b9f658a98f6c0f3743
Fitrahtur Rahman <fitrahtur@gmail.com>
2020-02-11 16:40:43 +0700

    change transcript

--- server/src/helpers/announcerTranscript.js
+++ server/src/helpers/announcerTranscript.js
@@ -3,11 +3,9 @@ function announcerTranscript(disaster, payload) {
 
   switch(disaster) {
     case 'earthquake':
-      const { aftershocks, depth, earthquake_point, magnitude, tsunami } = payload
-      text = `Mohon perhatian. Kami dari BMKG menginformasikan bahwa telah terjadi gempa bumi sebesar ${magnitude} Skala Rihter pada pukul 8 Waktu Indonesia Barat, dengan kedalaman ${depth} kilometer, di ${earthquake_point}. Dihimbau kepada warga agar waspada terhadap ${aftershocks ? 'gempa susulan' : ''}${tsunami ? ' dan bahaya tsunami' : ''}. Terima kasih.`
+      text = `Mohon perhatian. Kami dari BMKG menginformasikan bahwa telah terjadi gempa bumi sebesar ${payload.magnitude} Skala Rihter ${payload.tsunami ? ' dan berpotensi tsunami' : ''} pada tanggal 11 Februari 2020 pukul ${payload.time} Waktu Indonesia Barat, dengan kedalaman ${payload.depth} kilometer, di ${payload.earthquake_point}. Dihimbau kepada warga agar waspada terhadap gempa susulan. Terima kasih.`
     case 'flood':
-      const { lock, status, time, water_level, weather } = payload
-      text = `Mohon perhatian. Kami dari BPBD Provinsi DKI Jakarta menginformasikan bahwa pintu air ${lock} memasuki ${status} pada ${time} WIB dengan ketinggian ${water_level} sentimeter, ${weather}. Dihimbau kepada warga Bantaran Kali agar waspada banjir kiriman. Terima kasih.`
+      text = `Mohon perhatian. Kami dari BPBD Provinsi DKI Jakarta menginformasikan bahwa pintu air ${payload.lock} memasuki ${payload.status} pada tanggal 11 Februari 2020 pukul ${payload.time} Waktu Indonesia Barat dengan ketinggian ${payload.water_level} sentimeter, ${payload.weather}. ${payload.status.toLowerCase() === 'siaga 1' ? 'Dihimbau kepada warga Bantaran Kali agar segera melakukan evakuasi dan menyelamatkan barang berharga Anda' : 'Dihimbau kepada warga Bantaran Kali agar waspada banjir kiriman'}. Terima kasih.`
   }
   return text
 }
