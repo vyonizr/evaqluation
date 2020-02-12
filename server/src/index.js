dfac9777d3c1a7c593c6249558f82f419ed8a807
Fitrahtur Rahman <fitrahtur@gmail.com>
2020-02-12 08:12:40 +0700

    add conditional port

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const cors = require('cors')
-const PORT = 3000
+const PORT = process.env.PORT || 5000
 const routes = require('./routes')
 
 const app = express()
