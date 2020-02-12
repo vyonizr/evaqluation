443642b8a6fec8a58dbcb402293855cc57248616
Fitrahtur Rahman <fitrahtur@gmail.com>
2020-02-12 07:50:54 +0700

    fix payload status and break

--- server/src/helpers/announcerTranscript.js
+++ server/src/helpers/announcerTranscript.js
@@ -1,18 +1,15 @@
 function announcerTranscript(disaster, payload) {
   let text = ''
 
-  console.log(payload.status.toLowerCase() );
-
   switch(disaster) {
     case 'earthquake':
       text = `Mohon perhatian. Kami dari BMKG menginformasikan bahwa telah terjadi gempa bumi sebesar ${payload.magnitude} Skala Rihter ${payload.tsunami ? ' dan berpotensi tsunami' : ''} pada tanggal 11 Februari 2020 pukul ${payload.time} Waktu Indonesia Barat, dengan kedalaman ${payload.depth} kilometer, di ${payload.earthquake_point}. Dihimbau kepada warga agar waspada terhadap gempa susulan. Terima kasih.`
-      break
+      break;
     case 'flood':
-      text = `Mohon perhatian. Kami dari BPBD Provinsi DKI Jakarta menginformasikan bahwa ${payload.lock} memasuki ${payload.status} pada tanggal 11 Februari 2020 pukul ${payload.time} Waktu Indonesia Barat dengan ketinggian ${payload.water_level} sentimeter, ${payload.weather}. ${payload.status.toLowerCase() === 'siaga 1' ? 'Dihimbau kepada warga agar segera melakukan evakuasi dan menyelamatkan barang berharga Anda' : 'Dihimbau kepada warga agar waspada terhadap banjir kiriman'}. Terima kasih.`
-      break
+      text = `Mohon perhatian. Kami dari BPBD Provinsi DKI Jakarta menginformasikan bahwa ${payload.lock} memasuki ${payload.status} pada tanggal 11 Februari 2020 pukul ${payload.time} Waktu Indonesia Barat dengan ketinggian ${payload.water_level} sentimeter, ${payload.weather}. ${payload.status && payload.status.toLowerCase() === 'siaga 1' ? 'Dihimbau kepada warga agar segera melakukan evakuasi dan menyelamatkan barang berharga Anda' : 'Dihimbau kepada warga agar waspada terhadap banjir kiriman'}. Terima kasih.`
+      break;
     }
 
-    console.log(text);
   return text
 }
 
--- server/src/helpers/tweetContents.js
+++ server/src/helpers/tweetContents.js
@@ -4,8 +4,10 @@ function tweetContent(disaster, payload) {
   switch(disaster) {
     case 'earthquake':
       text = `Telah terjadi gempa bumi sebesar ${payload.magnitude} SR ${payload.tsunami ? ' dan berpotensi tsunami' : ''} pada tanggal 11 Februari 2020 pukul ${payload.time} WIB dengan kedalaman ${payload.depth} km di ${payload.earthquake_point}`
+      break
     case 'flood':
       text = `Pintu air ${payload.lock} memasuki ${payload.status} pada tanggal 11 Februari 2020 pukul ${payload.time} WIB dengan ketinggian ${payload.water_level} cm, ${payload.weather}. ${payload.status.toLowerCase() === 'siaga 1' ? 'Kami himbau agar segera melakukan evakuasi dan menyelamatkan barang berharga.' : 'Kami himbau agar waspada banjir kiriman'}`
+      break
   }
   return text
 }
