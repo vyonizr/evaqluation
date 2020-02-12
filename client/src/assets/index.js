2418a29532a5798df239def60b375b77eef7814d
Fitrahtur Rahman <fitrahtur@gmail.com>
2020-02-12 07:51:10 +0700

    add icon assets for earthquake and flood gate

---
+++ client/src/assets/index.js
@@ -0,0 +1,10 @@
+import evaqlueationLogoFull from './icons/evalueation_logo_full.svg'
+import iconEarthquake from './icons/icon_earthquake.png'
+import iconFloodGate from './icons/icon_floodgate.png'
+
+
+export {
+  evaqlueationLogoFull,
+  iconEarthquake,
+  iconFloodGate
+}
