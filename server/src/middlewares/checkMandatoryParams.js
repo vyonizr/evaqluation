e01545f339558ddc88cb8d5a52011aa4f3513b45
Fitrahtur Rahman <fitrahtur@gmail.com>
2020-02-13 06:29:45 +0700

    fix property check

--- server/src/middlewares/checkMandatoryParams.js
+++ server/src/middlewares/checkMandatoryParams.js
@@ -1,13 +1,13 @@
-module.exports = async (req, res, next) => {
+module.exports = (req, res, next) => {
   if (req.baseUrl === '/earthquakes') {
-    if (req.body.aftershocks
-    && req.body.depth
-    && req.body.earthquake_point
-    && req.body.magnitude
-    && req.body.tsunami
-    && req.body.time
-    && req.body.lat
-    && req.body.lng) {
+    if (req.body.hasOwnProperty('aftershocks')
+    && req.body.hasOwnProperty('depth')
+    && req.body.hasOwnProperty('earthquake_point')
+    && req.body.hasOwnProperty('magnitude')
+    && req.body.hasOwnProperty('tsunami')
+    && req.body.hasOwnProperty('time')
+    && req.body.hasOwnProperty('lat')
+    && req.body.hasOwnProperty('lng')) {
       next()
     }
     else {
@@ -17,13 +17,13 @@ module.exports = async (req, res, next) => {
     }
   }
   else if (req.baseUrl === '/floods') {
-    if (req.body.lock
-    && req.body.status
-    && req.body.time
-    && req.body.water_level
-    && req.body.weather
-    && req.body.lat
-    && req.body.lng) {
+    if (req.body.hasOwnProperty('lock')
+    && req.body.hasOwnProperty('status')
+    && req.body.hasOwnProperty('time')
+    && req.body.hasOwnProperty('water_level')
+    && req.body.hasOwnProperty('weather')
+    && req.body.hasOwnProperty('lat')
+    && req.body.hasOwnProperty('lng')) {
       next()
     }
     else {
