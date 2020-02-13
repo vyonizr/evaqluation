78bcda093251c5a06500de14bb428675d42c9b94
Fitrahtur Rahman <fitrahtur@gmail.com>
2020-02-13 18:33:22 +0700

    add date parser function for announcement

--- server/src/helpers/announcerTranscript.js
+++ server/src/helpers/announcerTranscript.js
@@ -1,13 +1,20 @@
+const dayjs = require('dayjs')
+require('dayjs/locale/id')
+
+function dateParser() {
+  return dayjs().locale('id').format('[pada tanggal] D MMMM YYYY [pukul] HH [lewat] m [menit] [Waktu Indonesia Barat]')
+}
+
 function announcerTranscript(disaster, payload) {
   let text = ''
 
   switch(disaster) {
     case 'earthquake':
       payload.magnitude = String(payload.magnitude).replace('.', ',')
-      text = `Mohon perhatian. Kami dari BMKG menginformasikan bahwa telah terjadi gempa bumi sebesar ${payload.magnitude} Skala Rihter ${payload.tsunami ? ' dan berpotensi tsunami' : ''} pada tanggal 11 Februari 2020 pukul ${payload.time} Waktu Indonesia Barat, dengan kedalaman ${payload.depth} kilometer, di ${payload.earthquake_point}. Dihimbau kepada warga agar waspada terhadap gempa susulan. Terima kasih.`
+      text = `Mohon perhatian. Kami dari BMKG menginformasikan bahwa telah terjadi gempa bumi sebesar ${payload.magnitude} Skala Rihter ${payload.tsunami ? ' dan berpotensi tsunami' : ''} ${dateParser()}, dengan kedalaman ${payload.depth} kilometer, di ${payload.earthquake_point}. Dihimbau kepada warga agar waspada terhadap gempa susulan. Terima kasih.`
       break;
     case 'flood':
-      text = `Mohon perhatian. Kami dari BPBD Provinsi DKI Jakarta menginformasikan bahwa ${payload.lock} memasuki ${payload.status} pada tanggal 11 Februari 2020 pukul ${payload.time} Waktu Indonesia Barat dengan ketinggian ${payload.water_level} sentimeter, ${payload.weather}. ${payload.status && payload.status.toLowerCase() === 'siaga 1' ? 'Dihimbau kepada warga agar segera melakukan evakuasi dan menyelamatkan barang berharga Anda' : 'Dihimbau kepada warga agar waspada terhadap banjir kiriman'}. Terima kasih.`
+      text = `Mohon perhatian. Kami dari BPBD Provinsi DKI Jakarta menginformasikan bahwa ${payload.lock} memasuki ${payload.status} ${dateParser()} dengan ketinggian ${payload.water_level} sentimeter, ${payload.weather}. ${payload.status && payload.status.toLowerCase() === 'siaga 1' ? 'Dihimbau kepada warga agar segera melakukan evakuasi dan menyelamatkan barang berharga Anda' : 'Dihimbau kepada warga agar waspada terhadap banjir kiriman'}. Terima kasih.`
       break;
     }
 
