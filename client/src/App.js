010c0627cf17e039daffdbf4f0ed72a915ab94df
Fitrahtur Rahman <fitrahtur@gmail.com>
2020-02-14 19:59:38 +0700

    add app description and disaster notification

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import L from 'leaflet'
 import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet'
+import Swal from 'sweetalert2'
 import 'firebase/auth'
 import 'firebase/firestore'
 import './App.css';
@@ -15,6 +16,13 @@ function App() {
   const [earthquakes, setEarthquakes] = useState([])
   const [floods, setFloods] = useState([])
   const [naturalDisasters, setNaturalDisasters] = useState([])
+  const [latestDisaster, setLatestDisaster] = useState({})
+  const [position, setPosition] = useState({
+    center: [-0.8, 120.9213],
+    zoomLevel: 4
+  })
+
+  const map = useRef()
 
   useEffect(() => {
     let earthquakesRef = firebaseDatabase.ref('earthquakes')
@@ -24,11 +32,17 @@ function App() {
         earthquakesArray.push(snapshot.val()[earthquake])
       }
 
-      if (firstEarthquakeCheck && earthquakesArray[earthquakesArray.length - 1]) {
-        let snd = new Audio("data:audio/wav;base64," + earthquakesArray[earthquakesArray.length - 1].base64);
-        snd.play();
+      const latestEarthquake = earthquakesArray[earthquakesArray.length - 1]
+      setLatestDisaster(latestEarthquake)
 
-        console.log(snd, '<== snd');
+      if (firstEarthquakeCheck && latestEarthquake) {
+        Swal.fire(
+          'Peringatan',
+          `Gempa bumi telah terjadi di ${latestEarthquake.lat}° ${latestEarthquake.lat < 0 ? 'LS' : 'LU'} ${latestEarthquake.lng}° ${latestEarthquake.lng < 0 ? 'BB' : 'BT'}`,
+          'warning'
+        )
+        let announcement = new Audio("data:audio/wav;base64," + latestEarthquake.base64);
+        announcement.play();
       }
 
       firstEarthquakeCheck = true
@@ -47,9 +61,16 @@ function App() {
         floodsArray.push(snapshot.val()[flood])
       }
 
-      if (firstFloodsCheck && floodsArray[floodsArray.length - 1]) {
-        let snd = new Audio("data:audio/wav;base64," + floodsArray[floodsArray.length - 1].base64);
-        snd.play();
+      const latestFlood = floodsArray[floodsArray.length - 1]
+
+      if (firstFloodsCheck && latestFlood) {
+        Swal.fire(
+          'Peringatan',
+          `${latestFlood.lock} telah memasuki ${latestFlood.status}`,
+          'warning'
+        )
+        let announcement = new Audio("data:audio/wav;base64," + latestFlood.base64);
+        announcement.play();
       }
 
       firstFloodsCheck = true
@@ -64,21 +85,25 @@ function App() {
     setNaturalDisasters(combinedDisasters)
   }, [earthquakes, floods])
 
+  useEffect(() => {
+    return () => firstEarthquakeCheck
+    && firstFloodsCheck
+    && setPosition({
+      center: [latestDisaster.lat, latestDisaster.lng],
+      zoomLevel: 13
+    })
+  }, [latestDisaster])
+
   const onMarkerClick = (e, marker) => {
   }
 
-  // const getFiles = (file) => {
-  //   console.log(file, '<== FILE');
-  // }
-
-  const position = [-0.8, 120.9213]
   return (
-    <div style={{height: "100%",width:"100%"}}>
-      <LeafletMap style={{height:'70vh'}} center={position} zoom={4}>
-      <TileLayer
-        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+    <div className='nunito' style={{width:'85%', marginTop: '.8rem', marginLeft: 'auto', marginRight:'auto'}}>
+      <LeafletMap ref={map} style={{height:'60vh'}} center={position.center} zoom={position.zoomLevel}>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
         <CanvasMarkersLayer
             dataKey='properties'
             onMarkerClick={(e, marker) => onMarkerClick(e, marker)}
@@ -87,7 +112,7 @@ function App() {
               naturalDisasters.map((disaster, index) => {
                 const latitude = Number(disaster.lat) || 0
                 const longitude = Number(disaster.lng) || 0
-                const markerIcon = disaster.magnitude ? iconEarthquake : iconFloodGate
+                const markerIcon = disaster.hasOwnProperty('magnitude') ? iconEarthquake : iconFloodGate
                 const defaultIcon = L.icon({
                   iconUrl: markerIcon,
                   iconSize: [32, 40],
@@ -107,7 +132,7 @@ function App() {
                         disaster={disaster}
                       >
                         <div>
-                          <h2>{ disaster.magnitude ? `Gempa Bumi ${disaster.magnitude} SR` : `Peringatan Banjir ${disaster.lock}: ${disaster.status}`}</h2>
+                          <h2>{ disaster.hasOwnProperty('magnitude') ? `Gempa Bumi ${disaster.magnitude} SR` : `Peringatan Banjir ${disaster.lock}: ${disaster.status}`}</h2>
                         </div>
                       </Popup>
                     </Marker>
@@ -116,9 +141,15 @@ function App() {
             )}
         </CanvasMarkersLayer>
       </LeafletMap>
-      <div style={{ height: '150px', margin: '15px 0px' }}>
+      <div style={{ height: '6rem', margin: '.5rem 0' }}>
         <img src={evaqlueationLogoFull} style={{width: '100%', height: '100%', objectFit: 'contain'}} alt='evaqlueation logo'/>
       </div>
+      <div style={{textAlign: 'justify', textAlignLast: 'left'}}>
+        <p>Evaqlueation is a simulator of an automatic early warning system that broadcasts real-time natural disaster information to help the government speeds up mitigation. It would trigger loudspeakers, display the disaster icon on the map, and post on social media (<a href='https://twitter.com/evaqlueation/' target="_blank" rel="noopener noreferrer">@evaqlueation</a>) when the sensor e.g. water level or seismometer reaches a value at a certain threshold.</p>
+      </div>
+      <footer style={{textAlign: 'center'}}>
+        &#169; {new Date().getFullYear()} Evaqlueation Team | <a href='https://github.com/vyonizr/evaqlueation' target="_blank" rel="noopener noreferrer">Github</a>
+      </footer>
     </div>
   );
 }
